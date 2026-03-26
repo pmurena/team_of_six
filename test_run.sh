1d656feee30254d652c7d5f6abf46853bdcccb8e
pmurena <patrick.murena@gmail.com>
2026-03-26 16:59:44 +0100

    added fixtures as part of the pr review

---
+++ test_run.sh
@@ -0,0 +1,13 @@
+cd ~/src/nvios
+git pull
+cat /run/team_of_six/pat_input.sh
+ls -la /mnt/team_of_six/sandbox/
+ls -la /mnt/team_of_six/sandbox/.tos_outbox
+tos wrapper
+cat /run/team_of_six/pat_input.sh
+ls -la /mnt/team_of_six/sandbox/.tos_outbox
+ls -la /mnt/team_of_six/sandbox/.tos_outbox/nvios
+tos publish
+ls -la /mnt/team_of_six/sandbox/.tos_outbox
+ls -la /mnt/team_of_six/sandbox/.tos_outbox/nvios
+gh issue list
